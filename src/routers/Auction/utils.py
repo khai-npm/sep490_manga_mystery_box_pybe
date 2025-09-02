f1338365f326f0f75662a55b0d30c6e72bab6489
Mirura <manhkhai2606@gmail.com>
2025-09-02 12:54:05 +0700

    hotfix : is_auction_session return 400

--- src/routers/Auction/utils.py
+++ src/routers/Auction/utils.py
@@ -410,6 +410,8 @@ async def action_is_joined_auction(current_user : str):
         joined_auction = await AuctionParticipant.find(AuctionParticipant.user_id==str(user_db.id)).to_list()
         for auction in joined_auction:
             session = await AuctionSession.find_one(AuctionSession.id == ObjectId(auction.auction_id))
+            if not session:
+                return False
             if session.start_time < datetime.now() < session.end_time:
                 return True
             
